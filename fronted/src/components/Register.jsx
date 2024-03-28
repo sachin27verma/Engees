78ebd1310292385d851a87487ec852b0722215d7
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2024-03-28 12:48:20 +0530

    fixed registration

--- fronted/src/components/Register.jsx
+++ fronted/src/components/Register.jsx
@@ -40,7 +40,7 @@ const Register = () => {
     const data = { username, password };
 
     try {
-      const response = await fetch("https://engeesserver.vercel.app/register", {
+      const response = await fetch("https://engeesserver.vercel.app/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
